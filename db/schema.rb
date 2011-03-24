1c0fe68e0d01f809c4875b87d5517a05a0c0e6da
Wayne Montague <wayne.montague@gmail.com>
2011-03-24 15:52:20 -0700

    Added db seed file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110307055638) do
+ActiveRecord::Schema.define(:version => 20110324071930) do
 
   create_table "assemblies", :force => true do |t|
     t.string   "project_name"
